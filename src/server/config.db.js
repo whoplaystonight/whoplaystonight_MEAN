585f599f223f551c1b001aa6fa47c5e722ba59f1
laramontava <laramontava@gmail.com>
2017-02-07 19:21:30 +0100

    Merge

--- src/server/config.db.js
+++ src/server/config.db.js
@@ -1,7 +1,7 @@
 var mysql=require('mysql');
 var connection=mysql.createConnection({
   host:'127.0.0.1',
-  user:'root',
+  user:'sergio',
   password:'1234',
   database:'Who_plays',
   port:'3306'
