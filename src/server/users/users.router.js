723a05d33360df0b66a5c255b62be0d8b27c5e36
laramontava <laramontava@gmail.com>
2017-02-13 23:15:03 +0100

    Change db

--- src/server/users/users.router.js
+++ src/server/users/users.router.js
@@ -9,10 +9,11 @@ module.exports = function (app) {
     //app.get('/api/loginFacebook', userscontroller.loginfacebook);
     app.get('/api/loginFacebook', passport.authenticate('facebook', { scope: ['email', 'public_profile'] }));
     //app.get('/api/auth/facebook/callback', userscontroller.loginfacebookcallback);
-    app.get('/api/auth/facebook/callback', passport.authenticate('facebook', {
-        successRedirect: '/locate',
-        failureRedirect: '/signin'
-    }));
+    app.get('/api/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/signin' }),
+        function (req, res) {
+            console.log('Facebook login ' + JSON.stringify(req.user));
+            res.redirect('/locate');
+        });
 
     app.get('/api/loginTwitter', passport.authenticate('twitter'));
     app.get('/api/auth/twitter/callback',
