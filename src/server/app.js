f73254b5defec068030be8e033c2c5c807ebcd89
sergiober73 <sergiober73@gmail.com>
2017-02-23 16:42:26 +0100

    Implement HTTPS protocol

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,10 @@
 /*jshint node:true*/
 'use strict';
 
+var fs = require('fs');
+var https = require('https');
+var http = require('http');
+var forceSSL = require('express-force-ssl');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -57,6 +61,18 @@ switch (environment) {
     });
     // Any deep link calls should return index.html
     app.use('/*', express.static('./build/index.html'));
+    console.log('WARNING: OPEN BROWSER WITH HTTPS');
+
+    https.createServer({
+      key: fs.readFileSync('server.key'),
+      cert:fs.readFileSync('server.crt')
+    }, app).listen(port);
+
+    app-use(forceSSL);//MODULE USED TO FORCE REDIRECTION
+    console.log('WARNING: BE CAREFULL, WE ARE TRYING TO LAUNCH SERVER ON PORT 80.' +
+                'CHECK IF ANY OTHER SERVER IS LISTENING ON SAME PORT (APACHE...)' +
+                'WE WANT TO FORCE HTTP TO HTTPS REDIRECTION ALWAYS');
+    http.createServer(app).listen(80);
     break;
   default:
     console.log('** DEV **');
