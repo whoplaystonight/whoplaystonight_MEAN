db0cd4a72850b91a36dbba0c02e44b1d35c4a478
sergiober73 <sergiober73@gmail.com>
2017-02-23 20:03:05 +0100

    FIx https protocol

--- src/server/app.js
+++ src/server/app.js
@@ -4,7 +4,7 @@
 var fs = require('fs');
 var https = require('https');
 var http = require('http');
-var forceSSL = require('express-force-ssl');
+// var forceSSL = require('express-force-ssl');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -68,11 +68,11 @@ switch (environment) {
       cert:fs.readFileSync('server.crt')
     }, app).listen(port);
 
-    app-use(forceSSL);//MODULE USED TO FORCE REDIRECTION
-    console.log('WARNING: BE CAREFULL, WE ARE TRYING TO LAUNCH SERVER ON PORT 80.' +
-                'CHECK IF ANY OTHER SERVER IS LISTENING ON SAME PORT (APACHE...)' +
-                'WE WANT TO FORCE HTTP TO HTTPS REDIRECTION ALWAYS');
-    http.createServer(app).listen(80);
+    // app-use(forceSSL);//MODULE USED TO FORCE REDIRECTION
+    // console.log('WARNING: BE CAREFULL, WE ARE TRYING TO LAUNCH SERVER ON PORT 80.' +
+    //             'CHECK IF ANY OTHER SERVER IS LISTENING ON SAME PORT (APACHE...)' +
+    //             'WE WANT TO FORCE HTTP TO HTTPS REDIRECTION ALWAYS');
+    // http.createServer(app).listen(80);
     break;
   default:
     console.log('** DEV **');
@@ -85,12 +85,19 @@ switch (environment) {
     });
     // Any deep link calls should return index.html
     app.use('/*', express.static('./src/client/index.html'));
+    app.listen(port, function () {
+      console.log('Express server listening on port ' + port);
+      console.log('env = ' + app.get('env') +
+      '\n__dirname = ' + __dirname +
+      '\nprocess.cwd = ' + process.cwd());
+    });
+
     break;
 }
 
-app.listen(port, function () {
-  console.log('Express server listening on port ' + port);
-  console.log('env = ' + app.get('env') +
-    '\n__dirname = ' + __dirname +
-    '\nprocess.cwd = ' + process.cwd());
-});
+// app.listen(port, function () {
+//   console.log('Express server listening on port ' + port);
+//   console.log('env = ' + app.get('env') +
+//     '\n__dirname = ' + __dirname +
+//     '\nprocess.cwd = ' + process.cwd());
+// });
